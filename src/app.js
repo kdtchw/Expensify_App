038639965dd685ce3ce59cc41103ab268164bc91
kdtchw <kdtchw@gmail.com>
2019-08-04 17:41:24 +0800

    Section 9 React-Router to Section 11 React with Redux

--- src/app.js
+++ src/app.js
@@ -1,9 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-import IndecisionApp from "./components/IndecisionApp";
-
+import { Provider } from "react-redux";
+import AppRouter from "./routers/AppRouter";
+import configureStore from "./store/configureStore";
+import { addExpense, removeExpense, editExpense } from "./actions/expenses";
+import { setTextFilter, sortByAmount, sortByDate, setStartDate, setEndDate } from "./actions/filters";
+import getVisibleExpenses from "./selectors/expenses";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
+const store = configureStore();
+
+store.subscribe(() => {
+    const state = store.getState();
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+    console.log(visibleExpenses);
+});
+
+store.dispatch(addExpense( { description: "Water bill", amount: 4500 } ));
+store.dispatch(addExpense( { description: "Gas bill", createdAt: 1000 } ));
+store.dispatch(addExpense( { description: "Rent", amount: 109500 } ));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById("app"));
